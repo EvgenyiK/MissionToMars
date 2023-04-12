4a23b2e19fb6e9e9be848b46336dbe8a3e8a48db
evgen <evgenprogrammer@gmail.com>
2023-04-12 15:49:17 +0300

    Добавьте файлы проекта.

---
+++ Source/MissionToMars/MissionToMars.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MissionToMars.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MissionToMars, "MissionToMars" );
+
+DEFINE_LOG_CATEGORY(LogMissionToMars)
+ 
