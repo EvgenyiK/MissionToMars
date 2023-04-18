728f224918bf867c66ef86ca00d9dd52d17fdf65
evgen <evgenprogrammer@gmail.com>
2023-04-18 20:00:44 +0300

    add cameracomponent

--- Source/MissionToMars/Public/Player/MissionToMarsCharacter.h
+++ Source/MissionToMars/Public/Player/MissionToMarsCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "MissionToMarsCharacter.generated.h"
 
+
+class UCameraComponent;
+
 UCLASS(Blueprintable)
 class AMissionToMarsCharacter : public ACharacter
 {
@@ -14,21 +17,20 @@ class AMissionToMarsCharacter : public ACharacter
 public:
 	AMissionToMarsCharacter();
 
+	
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	virtual void BeginPlay() override;
+
+
+public:
 	// Called every frame.
 	virtual void Tick(float DeltaSeconds) override;
 
-	/** Returns TopDownCameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent);
 
-private:
-	/** Top down camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* TopDownCameraComponent;
-
-	/** Camera boom positioning the camera above the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
 };
 
