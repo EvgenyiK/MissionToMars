7380f92b4a13b6d7db498ea4dc2bc59d0b38ef01
evgen <evgenprogrammer@gmail.com>
2023-04-19 19:26:34 +0300

    add camera

--- Source/MyProject/MyProjectGameModeBase.cpp
+++ Source/MyProject/MyProjectGameModeBase.cpp
@@ -2,4 +2,11 @@
 
 
 #include "MyProjectGameModeBase.h"
+#include "Player/BaseCharacter.h"
+#include "Player/BasePlayerController.h"
 
+AMyProjectGameModeBase::AMyProjectGameModeBase()
+{
+	DefaultPawnClass = ABaseCharacter::StaticClass();
+	PlayerControllerClass = ABasePlayerController::StaticClass();
+}
