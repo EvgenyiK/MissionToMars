a1a24ea59ddf879f4e3eec99a84ca5f5e4269d39
evgen <evgenprogrammer@gmail.com>
2023-06-14 16:37:06 +0300

    shoot to enemy command

--- Source/MyProject/Private/Component/MAIPerceptionComponent.cpp
+++ Source/MyProject/Private/Component/MAIPerceptionComponent.cpp
@@ -23,7 +23,11 @@ AActor* UMAIPerceptionComponent::GetClosesEnemy() const
 	for (auto PercieveActor : PercieveActors)
 	{
 		const auto HealthComponent = MUtils::GetMPlayerComponent<UBaseHealthComponent>(PercieveActor);
-		if (HealthComponent && !HealthComponent->IsDead()) //TODO: check if enemies or not
+
+		const auto PercievePawn = Cast<APawn>(PercieveActor);
+		const auto AreEnemies = PercievePawn && MUtils::AreEnemies(Controller, PercievePawn->Controller);
+		
+		if (HealthComponent && !HealthComponent->IsDead() && AreEnemies)
 		{
 			const auto CurrentDistance = (PercieveActor->GetActorLocation() - Pawn->GetActorLocation()).Size();
 			if (CurrentDistance < BestDistance)
